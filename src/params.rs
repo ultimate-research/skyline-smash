24e9dde98687868fc2a6fbef041b29c0e5f9b028
jugeeya <jugeeya@live.com>
2020-09-09 19:56:37 -0700

    add params hook helper structs

--- src/params.rs
+++ src/params.rs
@@ -9,6 +9,53 @@ pub struct Params {
     pub vtable: u64,
 }
 
+#[derive(Debug)]
+pub struct ParamsInfo<'a> {
+    pub object_ptr: &'a u64,
+    pub filepath_hash: &'a u64,
+}
+
+impl<'a> ParamsInfo<'a> {
+    // A function only to be used by param_hook in order to construct ParamsInfos. Since fields may be
+    /// added, this API is subject to change.
+    #[cfg(feature = "params_internal")]
+    pub fn new(object_ptr: &'a u64, filepath_hash: &'a u64) -> Self {
+        Self {
+            object_ptr,
+            filepath_hash,
+        }
+    }
+
+    pub fn is_type<T: Filepath>(&self) -> bool {
+        crate::hash40(T::filepath()) == *self.filepath_hash
+    }
+}
+
+pub type Callback = fn(&ParamsInfo);
+
+/// An error representing the NRO hook plugin not successfully being linked against
+#[derive(Debug, Clone, Copy)]
+pub struct ParamsHookPluginMissing;
+
+#[allow(improper_ctypes)]
+extern "C" {
+    fn add_param_load_hook(callback: Callback);
+}
+
+/// A function to allow adding a hook for immediately after an NRO has been loaded.
+///
+/// **Note:** Requires the Param hook plugin. Will return an error otherwise.
+pub fn add_hook(callback: Callback) -> Result<(), ParamsHookPluginMissing> {
+    if (add_param_load_hook as *const ()).is_null() {
+        Err(ParamsHookPluginMissing)
+    } else {
+        unsafe {
+            add_param_load_hook(callback);
+        }
+        Ok(())
+    }
+}
+
 pub trait Filepath {
     fn filepath() -> &'static str;
 }
