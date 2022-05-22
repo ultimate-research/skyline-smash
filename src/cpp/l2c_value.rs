0c4bf7000ef442d89335256bf950747653da72b3
jugeeya <jugeeya@live.com>
2022-05-22 15:26:32 -0700

    Remove cache logic (for global stored LuaConsts usage)

--- src/cpp/l2c_value.rs
+++ src/cpp/l2c_value.rs
@@ -361,23 +361,15 @@ impl core::ops::Deref for LuaConst {
     fn deref(&self) -> &Self::Target {
         let cache = self.cache.get();
 
-        if let Some(ref val) = unsafe { *cache }  {
+        let mut val : i32 = -1;
+        if unsafe { lib::lua_bind_get_value(self.lua_bind_hash, &mut val) } {
             unsafe {
-                // if there is a bug with this implementation
-                // DEFINITELY start here
-                core::mem::transmute(val)
-            }
-        } else {
-            let mut val : i32 = -1;
-            if unsafe { lib::lua_bind_get_value(self.lua_bind_hash, &mut val) } {
-                unsafe {
-                    *cache = Some(val);
+                *cache = Some(val);
 
-                    (*cache).as_ref().unwrap()
-                }
-            } else{
-                &-1
+                (*cache).as_ref().unwrap()
             }
+        } else{
+            &-1
         }
     }
 }
